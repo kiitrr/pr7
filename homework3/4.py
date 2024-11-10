55076bd0fe6c8b193a22aa79cb10b0e4c5f5ceb1
kiitrr <renatuz10@gmail.com>
2024-11-10 20:43:37 +0500

    Update 4.py

--- homework3/4.py
+++ homework3/4.py
@@ -1,8 +1,18 @@
 while True:
-    decimal_input = input("Введите десятичное число")
+    decimal_input = input("Введите десятичное число: ")
     
     if decimal_input.isdigit() and int(decimal_input) >= 0:
         decimal_number = int(decimal_input)
         break
     else:
         print("Ошибка: введено некорректное число. Пожалуйста, введите положительное целое число.")
+
+result = ""
+if decimal_number == 0:
+    result = "0"
+else:
+    while decimal_number > 0:
+        result = str(decimal_number % 3) + result
+        decimal_number //= 3
+
+print(f"Число в троичной системе счисления: {result}")
