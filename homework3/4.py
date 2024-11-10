19b89c7b861bc51a4556913aeb50eef8eac3c768
kiitrr <renatuz10@gmail.com>
2024-11-10 20:43:04 +0500

    Create 4.py

---
+++ homework3/4.py
@@ -0,0 +1,8 @@
+while True:
+    decimal_input = input("Введите десятичное число: ")
+    
+    if decimal_input.isdigit() and int(decimal_input) >= 0:
+        decimal_number = int(decimal_input)
+        break
+    else:
+        print("Ошибка: введено некорректное число. Пожалуйста, введите положительное целое число.")
